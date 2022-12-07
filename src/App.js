74ee01aed415f2e03fccd90542c5423ffc0d2499
JagrutiMahajan04 <jagrutimaha2000@gmail.com>
2022-12-07 16:39:52 +0530

    emojikit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [emoji,setEmoji] = useState("🎄")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='container'>
+    <div className='app-title-container'>
+    <h1 className='app-title'>🎅CHRISTMAS EMOJI KIT🎁</h1>
     </div>
+
+    <div className='main-emoji-container'>
+      <div className='main-emoji'>
+           {emoji}
+      </div>
+    </div>
+
+    <div className='emoji-container'>
+      <div className='emoji-item' onClick={()=>{setEmoji('🎄')}}>🎄</div>
+      <div className='emoji-item' onClick={()=>{setEmoji('⛄')}}>⛄</div>
+      <div className='emoji-item' onClick={()=>{setEmoji('🎅')}}>🎅</div>
+      <div className='emoji-item' onClick={()=>{setEmoji('🎁')}}>🎁</div>
+      <div className='emoji-item' onClick={()=>{setEmoji('💒')}}>💒</div>
+      <div className='emoji-item' onClick={()=>{setEmoji('🍰')}}>🍰</div>
+
+
+
+    </div>
+   </div>
   );
 }
 
