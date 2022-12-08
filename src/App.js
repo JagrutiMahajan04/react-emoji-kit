8d77bdd2e20ecdb2c3f38f37d88c193bb9c8a4e6
JagrutiMahajan04 <jagrutimaha2000@gmail.com>
2022-12-08 16:05:10 +0530

    size and rotate

--- src/App.js
+++ src/App.js
@@ -4,31 +4,61 @@ import './App.css';
 
 function App() {
 
-  const [emoji,setEmoji] = useState("🎄")
+  const [emoji, setEmoji] = useState("🎄")
+  const [emojiColor,setEmojiColor] = useState("white")
+  const [emojiSize,setEmojiSize] = useState(150);
+  const [emojiDeg, setEmojiDeg] = useState(0);
+
   return (
-   <div className='container'>
-    <div className='app-title-container'>
-    <h1 className='app-title'>🎅CHRISTMAS EMOJI KIT🎁</h1>
-    </div>
-
-    <div className='main-emoji-container'>
-      <div className='main-emoji'>
-           {emoji}
+    <div className='container'>
+      <div className='app-title-container'>
+        <h1 className='app-title'>🎅CHRISTMAS EMOJI KIT🎁</h1>
       </div>
+
+
+      <div className='emoji-editor-container'>
+        <div>
+          <div className='main-emoji-container' style={{ backgroundColor: emojiColor,transform: `rotate(${emojiDeg*3.6}deg)`}}>
+            <div className='main-emoji' style={{fontSize:`${emojiSize*2}px`}}>
+              {emoji}
+            </div>
+          </div>
+
+          <div className='emoji-container'>
+            <div className='emoji-item' onClick={() => { setEmoji('🎄') }}>🎄</div>
+            <div className='emoji-item' onClick={() => { setEmoji('⛄') }}>⛄</div>
+            <div className='emoji-item' onClick={() => { setEmoji('🎅') }}>🎅</div>
+            <div className='emoji-item' onClick={() => { setEmoji('🎁') }}>🎁</div>
+            <div className='emoji-item' onClick={() => { setEmoji('💒') }}>💒</div>
+            <div className='emoji-item' onClick={() => { setEmoji('🍰') }}>🍰</div>
+          </div>
+        </div>
+
+        <div>
+          <div className='colors-container'>
+            <h4>Bg-Color:</h4>
+            <div className='color-item bg-red' onClick={() => { setEmojiColor("red") }}></div>
+            <div className='color-item bg-blue' onClick={() => { setEmojiColor("blue") }}></div>
+            <div className='color-item bg-yellow' onClick={() => { setEmojiColor("yellow") }}></div>
+            <div className='color-item bg-pink' onClick={() => { setEmojiColor("pink") }}></div>
+          </div>
+
+          <div className='size-container'>
+            <h4>Size:</h4>
+            <input type= "range" className='size-slider' onChange={(e)=>{setEmojiSize(e.target.value)}}></input> 
+          </div>
+
+          <div className='size-container'>
+            <h4>Rotate:</h4>
+            <input type= "range" className='size-slider' onChange={(e)=>{setEmojiDeg(e.target.value)}}></input> 
+          </div>
+
+        </div>
+
+      </div>
+
+
     </div>
-
-    <div className='emoji-container'>
-      <div className='emoji-item' onClick={()=>{setEmoji('🎄')}}>🎄</div>
-      <div className='emoji-item' onClick={()=>{setEmoji('⛄')}}>⛄</div>
-      <div className='emoji-item' onClick={()=>{setEmoji('🎅')}}>🎅</div>
-      <div className='emoji-item' onClick={()=>{setEmoji('🎁')}}>🎁</div>
-      <div className='emoji-item' onClick={()=>{setEmoji('💒')}}>💒</div>
-      <div className='emoji-item' onClick={()=>{setEmoji('🍰')}}>🍰</div>
-
-
-
-    </div>
-   </div>
   );
 }
 
